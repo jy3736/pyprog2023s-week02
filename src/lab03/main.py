440e391dd67d10708b2d7286902a3191b91775d3
jy3736 <jyang@stust.edu.tw>
2023-02-17 06:49:19 +0800

    jyang

--- src/lab03/main.py
+++ src/lab03/main.py
@@ -4,7 +4,7 @@ def main():
     time_list1 = time1.split()
     time_list2 = time2.split()
     hours1 = int(time_list1[0])
-    minutes1 = int(time_list1[1])
+    minutes1 = int(time_list1[0])
     seconds1 = int(time_list1[2])
     hours2 = int(time_list2[0])
     minutes2 = int(time_list2[1])
@@ -12,8 +12,9 @@ def main():
     total_seconds1 = hours1 * 3600 + minutes1 * 60 + seconds1
     total_seconds2 = hours2 * 3600 + minutes2 * 60 + seconds2
     if total_seconds2 < total_seconds1:
-        return -1
-    return total_seconds2 - total_seconds1
+        print(-1)
+    else:
+        print(total_seconds2 - total_seconds1)
 
 if __name__ == '__main__':
-    print(main())
+    main()
--- tools/chk_lab03.py
+++ tools/chk_lab03.py
@@ -1,5 +1,7 @@
 # Do not modify this file.  It is used by the autograder.
 
+# Do not modify this file.  It is used by the autograder.
+
 import unittest
 from io import StringIO
 from unittest.mock import patch
@@ -16,25 +18,19 @@ from main import main
 class TestMain(unittest.TestCase):
     
     def test_time_diff(self):
-        test_cases = [("1 30 15\n2 0 10\n", 1795), 
-                      ("5 15 30\n5 15 31\n", 1),
-                      ("10 30 20\n8 15 5\n", -1),
-                      ("0 0 0\n0 0 1\n", 1),
-                      ("0 0 0\n1 0 0\n", 3600),
-                      ("0 0 0\n23 59 59\n", 86399),
-                      ("23 59 59\n0 0 0\n", -1)]
+        test_cases = [("1 30 15\n2 0 10\n", "1795\n"), 
+                      ("5 15 30\n5 15 31\n", "1\n"),
+                      ("10 30 20\n8 15 5\n", "-1\n"),
+                      ("0 0 0\n0 0 1\n", "1\n"),
+                      ("0 0 0\n1 0 0\n", "3600\n"),
+                      ("0 0 0\n23 59 59\n", "86399\n"),
+                      ("23 59 59\n0 0 0\n", "-1\n")]
         for test_input, expected_output in test_cases:
             with patch('sys.stdin', StringIO(test_input)):
-                try:
-                    result = main()
-                    self.assertEqual(result, expected_output)
-                except AssertionError as e:
-                    print(f"Failed test case:\n{test_input.strip()}\n expected = {expected_output}, actual = {result}")
-                    return -1
-        return 0
-                
+                with patch('sys.stdout', new=StringIO()) as result:
+                    print(f"Testing case:\n{test_input}" )
+                    main()
+                    self.assertEqual(result.getvalue(), expected_output)
+
 if __name__ == '__main__':
     unittest.main(argv=[''], exit=False)
-
-
-
